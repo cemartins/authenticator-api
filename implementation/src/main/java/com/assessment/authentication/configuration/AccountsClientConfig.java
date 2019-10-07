b9e2e5f2a68724f2e9a20fa2da9060c842f8209e
Carlos Martins <carlos.mfa.martins@gmail.com>
2019-10-07 22:20:10 +0200

    different approach to tls client (err)

--- implementation/src/main/java/com/assessment/authentication/configuration/AccountsClientConfig.java
+++ implementation/src/main/java/com/assessment/authentication/configuration/AccountsClientConfig.java
@@ -1,19 +1,32 @@
 package com.assessment.authentication.configuration;
 
+import ch.qos.logback.core.net.ssl.KeyStoreFactoryBean;
+import org.apache.catalina.webresources.FileResource;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.http.HttpClient;
 import java.security.*;
 import java.security.cert.CertificateException;
 
@@ -38,50 +51,50 @@ public class AccountsClientConfig {
     @Value("${server.ssl.trust-store-password}")
     private String truststorePassword;
 
-    @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        KeyStore keyStore;
-        KeyStore trustStore;
-        HttpComponentsClientHttpRequestFactory requestFactory = null;
-
-        try {
-            keyStore = KeyStore.getInstance(keystoreType);
-            InputStream inputStream = this.getClass().getResourceAsStream(keystorePath);
-            keyStore.load(inputStream, keystorePassword.toCharArray());
-
-            trustStore = KeyStore.getInstance(truststoreType);
-            InputStream trustInputStream = this.getClass().getResourceAsStream(truststorePath);
-            trustStore.load(trustInputStream, truststorePassword.toCharArray());
-
-            SSLContextBuilder contextBuilder = new SSLContextBuilder();
-            contextBuilder.setProtocol("TLS");
-            contextBuilder.loadKeyMaterial(keyStore, keystorePassword.toCharArray());
-            contextBuilder.setKeyStoreType(keystoreType);
-            contextBuilder.loadTrustMaterial(trustStore, new TrustAllStrategy());
-
-            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(contextBuilder.build());
-
-            final CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
-
-            requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-            requestFactory.setConnectTimeout(10000); // 10 seconds
-            requestFactory.setReadTimeout(10000); // 10 seconds
-
-            restTemplate.setRequestFactory(requestFactory);
-
-        } catch (KeyStoreException e) {
-            throw new BeanInitializationException("Cannot instantiate keystore", e);
-        } catch (IOException e) {
-            throw new BeanInitializationException("Cannot read keystore path", e);
-        } catch (CertificateException | NoSuchAlgorithmException e) {
-            throw new BeanInitializationException("Cannot load keystore", e);
-        } catch (UnrecoverableKeyException | KeyManagementException e) {
-            throw new BeanInitializationException("Cannot create socket factory", e);
-        }
-
-        return restTemplate;
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(createHttpClient(applicationKeyAlias)));
     }
 
+    private static Resource makeResource(final String path) {
+        if (path.startsWith(FILE_RESOURCE_PREFIX)) {
+            return new FileResource(new File(path.substring(FILE_RESOURCE_PREFIX.length())));
+        } else if (path.startsWith(CLASSPATH_RESOURCE_PREFIX)) {
+            return new ClassPathResource(path.substring(CLASSPATH_RESOURCE_PREFIX.length()));
+        }
+        // Assume a path without a known prefix is a file
+        return new FileResource(new File(path));
+    }
+
+    private HttpClient createHttpClient(final String keyAlias) {
+        logger.info("Creating HTTP client using keystore={} and alias={}", keystorePath, keyAlias);
+        final KeyStore trustStore = new KeyStoreFactoryBean(
+                makeResource("classpath:/truststore.jks"), "JKS", "changeit").newInstance();
+        final KeyStore keyStore = new KeyStoreFactoryBean(
+                makeResource(keystorePath), keystoreType, keystorePassword).newInstance();
+        final SSLContext sslContext;
+        try {
+            sslContext = SSLContexts.custom()
+                    .loadKeyMaterial(keyStore, keystorePassword.toCharArray(), (aliases, socket) -> keyAlias)
+                    .loadTrustMaterial(trustStore, (x509Certificates, s) -> false)
+                    .build();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException | UnrecoverableKeyException e) {
+            throw new IllegalStateException("Error loading key or trust material", e);
+        }
+        final SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(
+                sslContext,
+                new String[] { "TLSv1.2" },
+                null,
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+        final Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", sslSocketFactory)
+                .build();
+        final PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+        connectionManager.setMaxTotal(httpClientPoolSize);
+        connectionManager.setDefaultMaxPerRoute(httpClientPoolSize);
+        return HttpClients.custom()
+                .setSSLSocketFactory(sslSocketFactory)
+                .setConnectionManager(connectionManager)
+                .build();
+    }
 }
